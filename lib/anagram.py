e47b6b0956ecea6379584f9ce4da1855da6e92e1
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-05-25 18:45:01 +0300

    anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+ 
+
+class Anagram:
+    def __init__(self, anagram):
+        self.anagram = anagram
+    def match (self, words):
+       sorted_word = sorted(self.anagram)
+       return [word for word in words if sorted(word)== sorted_word]
+
